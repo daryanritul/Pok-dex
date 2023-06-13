b700377d3b694b77de4e9a50da2237476ecba562
Ritul Daryan <daryanritul@gmail.com>
2023-06-13 17:53:10 +0530

    Store/Router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { context } from './store/store';
+
+import HomePage from './Screens/HomePage/HomePage';
 
 const App = () => {
   return (
-    <div>
-      <p>Pokedex</p>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 };
--- src/RootApp.js
+++ src/RootApp.js
@@ -1,7 +1,24 @@
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
+import { context, initialState } from './store/store';
+import { useReducer } from 'react';
+import reducer from './store/reducer';
 
 const RootApp = () => {
-  return <App />;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <BrowserRouter>
+      <context.Provider
+        value={{
+          state,
+          dispatch,
+        }}
+      >
+        <App />
+      </context.Provider>
+    </BrowserRouter>
+  );
 };
 
 export default RootApp;
