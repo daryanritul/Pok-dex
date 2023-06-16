706a7b1f0df5c144754bde439f23db351380c63c
Ritul Daryan <daryanritul@gmail.com>
2023-06-16 11:08:01 +0530

    Bookmark/Search Screeen Done-UI

--- src/Screens/BookmarkPage/BookmarkPage.js
+++ src/Screens/BookmarkPage/BookmarkPage.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import sty from './BookmarkPage.module.css';
+
+import PokemonCard from '../../Components/PokemonCard/PokemonCard';
+import Dropdown from '../../Components/Dropdown/Dropdown';
 
 const BookmarkPage = () => {
-  return <div>BookmarkPage</div>;
+  return (
+    <div className="body">
+      <div className={sty.filterTitle}>My Bookmarks</div>
+      <div className={sty.pokeBox}>
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+      </div>
+    </div>
+  );
 };
 
 export default BookmarkPage;
--- src/Screens/SearchPage/SearchPage.js
+++ src/Screens/SearchPage/SearchPage.js
@@ -1,7 +1,53 @@
 import React from 'react';
+import sty from './SearchPage.module.css';
+import { MdSearch } from 'react-icons/md';
+import PokemonCard from '../../Components/PokemonCard/PokemonCard';
+import Dropdown from '../../Components/Dropdown/Dropdown';
 
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <div className={sty.page}>
+      <div className={sty.searchBox}>
+        <h2>
+          What <h1>Pokémon</h1> are you looking for?
+        </h2>
+        <div className={sty.search}>
+          <MdSearch className={sty.searchIcon} />
+          <input
+            type="text"
+            className=""
+            placeholder="Search Pokémon, Move, Ability etc "
+          />
+        </div>
+      </div>
+      <div className="body">
+        <div className={sty.filters}>
+          <div className={sty.filterTitle}>Apply Filters</div>
+          <Dropdown title={'Colors'} options={['red', 'green', 'blue']} />
+          <Dropdown title={'Colors'} options={['red', 'green', 'blue']} />
+          <Dropdown title={'Colors'} options={['red', 'green', 'blue']} />
+          <Dropdown title={'Colors'} options={['red', 'green', 'blue']} />
+          <Dropdown title={'Colors'} options={['red', 'green', 'blue']} />
+          <button className={sty.apply}>Apply</button>
+          <button>Clear</button>
+        </div>
+        <div className={sty.filterTitle}>Serach Results : Ditto</div>
+        <div className={sty.pokeBox}>
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
